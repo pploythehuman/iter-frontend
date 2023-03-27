7665e5c8eaf3cf9ffc8b59313cf98fccadb73906
pploythehuman <kanokpon26@gmail.com>
2023-03-27 23:01:08 +0700

    add api key as environment variable

--- src/components/maps/GoogleMaps.tsx
+++ src/components/maps/GoogleMaps.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { GoogleMap as GoogleMapComponent, LoadScript, Marker } from '@react-google-maps/api';
-require("dotenv").config();
 
 const containerStyle = {
   width: '100%',
@@ -18,8 +17,7 @@ const GoogleMap: React.FC<GoogleMapProps> = () => {
   console.log("key", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string}>
-     {/* <LoadScript googleMapsApiKey={"AIzaSyC4w4El8szxw-8l_05Dvc7hlZjdEtiQsOE"}>  */}
+     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string}>
       <GoogleMapComponent mapContainerStyle={containerStyle} center={center} zoom={10}>
         <Marker position={center} />
       </GoogleMapComponent>
