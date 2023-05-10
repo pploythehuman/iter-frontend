839a1c5e98fc25987610775050f57fc88392fbc9
pploythehuman <kanokpon26@gmail.com>
2023-05-10 16:12:52 +0700

    add api for itinerary page

---
+++ src/services/itinerary/index.ts
@@ -0,0 +1,10 @@
+import { apiGet, apiPost } from '../api';
+
+const getItinerary = async (data: any) => {
+  // const response = await apiPost<any, { message: string }>('user/register/', data);
+  // return response;
+};
+
+export {
+  getItinerary,
+};
