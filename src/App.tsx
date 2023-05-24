ef9371a70a3d8532de3116095df66bcbbd123575
pploythehuman <kanokpon26@gmail.com>
2023-05-24 19:17:19 +0700

    add link not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import logo from './logo.svg';
+import { Button, Result } from 'antd';
 import './App.css';
-import Layout from './components/Layout'
+import Layout from './components/Layout';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import SignInPage from './pages/Login';
 import SignupPage from './pages/Register';
 import Itinerary from './pages/Itinerary';
 
-//drag and drop
-// import { DndProvider } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
-
-
 function App() {
   return (
-    // <DndProvider backend={HTML5Backend}>
     <BrowserRouter>
     <Layout>
       <Routes>
@@ -33,11 +28,25 @@ function App() {
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route> */}
+        <Route path="*" element={<NotFound/>} />
       </Routes>
       </Layout>
     </BrowserRouter>
-    // </DndProvider>
   );
 }
 
 export default App;
+
+const NotFound = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+    <Navbar />
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary" href="/">Back Home</Button>}
+    />
+    </div>
+  );
+};
